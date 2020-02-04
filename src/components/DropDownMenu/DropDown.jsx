27dee6a26754af8fefe02b86cfb83b3e46766cbe
Ogen86 <ogen86@gmail.com>
2020-02-04 11:18:08 +0100

    Update DropDown.jsx

--- src/components/DropDownMenu/DropDown.jsx
+++ src/components/DropDownMenu/DropDown.jsx
@@ -12,10 +12,10 @@ const DropDown = props => {
     <nav className={dropDownClasses}>
       <ul>
         <li>
-          <Link to="Dice_generation">dice generator</Link>
+          <Link to="/Dice_generation">dice generator</Link>
         </li>
         <li>
-          <Link to="News">news</Link>
+          <Link to="/News">news</Link>
         </li>
       </ul>
     </nav>
