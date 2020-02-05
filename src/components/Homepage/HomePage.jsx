b4de6119e40d836fdac63e1a76180f3a6ce37cd8
Ogen86 <ogen86@gmail.com>
2020-02-05 23:49:38 +0100

    homepage style updated

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
+import "font-awesome/css/font-awesome.min.css";
+
+import "./HomePage.css";
+import codePic from "../pictures/css-code-pic.jpg";
 
 class HomePage extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>HomePage</h1>
-        <h2>Work in progress!</h2>
+      <div className="card-container">
+        <div className="spacer"></div>
+        <div className="card">
+          <div className="card-front">
+            <img src={codePic} />
+          </div>
+          <div className="card-back">
+            <div className="card-details">
+              <h2 className="card-header">Köller Bálint</h2>
+              <span className="card-span">Junior fejlesztő</span>
+              <div className="card-social-details">
+                <a
+                  className="social-icons"
+                  href="https://www.facebook.com/balint.koller.9"
+                >
+                  <i class="fa fa-facebook" aria-hidden="true"></i>
+                </a>
+                <a className="social-icons" href="https://github.com/Ogen86">
+                  <i class="fa fa-github" aria-hidden="true"></i>
+                </a>
+                <a
+                  className="social-icons"
+                  href="https://www.linkedin.com/in/b%C3%A1lint-k%C3%B6ller-2a0a1697/"
+                >
+                  <i class="fa fa-linkedin" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="spacer"></div>
       </div>
     );
   }
